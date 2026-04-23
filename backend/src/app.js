ab4d3b4650e963c1975e11a9d13ddde7b05fc63f
Mantra Patel <patelmantra551@gmail.com>
2026-04-23 23:38:22 +0530

    added proj apis for create/list/rotate-key

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const eventRoutes = require('./routes/eventRoutes');
 const sessionRoutes = require('./routes/sessionRoutes');
+const projectRoutes = require('./routes/projectRoutes');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use('/api/events', eventRoutes);
 app.use('/api/sessions', sessionRoutes);
+app.use('/api/projects', projectRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the LivePulse API');
